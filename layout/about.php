5495fc547adcf54ebb79af544ef8d5a61712e63e
LeylaMolnar <leyla.molnar24@gmail.com>
2023-02-01 10:54:33 +0200

    Sample

--- layout/about.php
+++ layout/about.php
@@ -1,5 +1,5 @@
 <?php
-$title = "Amongoose";
+$title = "Amongooooose";
 include "header.php" ?>
 <h2>among us</h2>
 <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Rem sint, minus repellat, animi libero commodi vero eius earum, aliquam tempore ratione distinctio hic voluptate veniam? Ducimus pariatur enim placeat doloremque.</p>
