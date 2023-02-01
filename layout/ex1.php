baef02d01f959b81dfd51296f8b1cb06c3a32f4b
LeylaMolnar <leyla.molnar24@gmail.com>
2023-02-01 11:23:00 +0200

    Exercise 1 complete

--- layout/ex1.php
+++ layout/ex1.php
@@ -2,16 +2,46 @@
 $title = "Exercise 1";
 include "header.php"?>
 
-    <h1>3.1 Write a simple PHP script to print your information (Name and your groupid)</h1>
+    <h3>3.1 Write a simple PHP script to print your information (Name and your groupid)</h3>
     <?php
     echo "Leyla Molnár<br>BBCAP22";
     ?>
-    <h1>3.2 Write PHP code to display the following message</h1>
+    <h3>3.2 Write PHP code to display the following message</h3>
     <?php
     echo "Hello world! My name is \"Leyla\"";
     ?>
-    <h1>3.3 Write the PHP code in to display the current date</h1>
+    <h3>3.3 Write the PHP code in to display the current date</h3>
     <?php
     echo date("d.m.Y");
     ?>
+    <h3>3.4 Variables in php </h3>
+    <?php
+    $title1 = "PHP is interesting";
+    echo "<h1>" . $title1 . "</h1";
+    ?>
+
+    <h3>3.5 Tables & Variables</h3>
+    <?php
+    $g1 = 5;
+    $g2 = 4;
+    $g3 = 5;
+    echo "
+    <table>
+    <tr>
+    <th>S.N</th><th>Name</th><th>Grade</th>
+    </tr>
+    <tr>
+    <td>1</td><td>Pekka</td><td>$g1</td>
+    </tr>
+    <tr>
+    <td>2</td><td>Johanna</td><td>$g2</td>
+    </tr>
+    <tr>
+    <td>3</td><td>John</td><td>$g3</td>
+    </tr>
+    </table>
+    ";
+    ?>
+    <h3>4 Screenshot of development environment </h3>
+    <img src="images/ss.png" alt="Dev envirnomment screenshot" width="80%">
 <?php include "footer.php"?>
